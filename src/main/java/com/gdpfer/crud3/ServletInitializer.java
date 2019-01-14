f53cdc207233af1cdf6c7295473a9ebc808954d9
fernando <Fernando.Gomez3@wnco.com>
2019-01-14 14:08:34 -0600

    first commit

---
+++ src/main/java/com/gdpfer/crud3/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.gdpfer.crud3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Crud3Application.class);
+	}
+
+}
+
---
+++ src/test/java/com/gdpfer/crud3/Crud3ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gdpfer.crud3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Crud3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
